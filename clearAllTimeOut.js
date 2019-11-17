e43ad26efa21ce18e034042c320b71b934fc9e42
lanhoter <lanhoter@hotmail.com>
2019-11-17 10:31:19 +0000

    Init

---
+++ clearAllTimeOut.js
@@ -0,0 +1,27 @@
+// Question 2:
+// Implement clearAllTimeout
+
+let id = setTimeout(callback, delay);
+clearTimeout(id);
+
+setTimeout(callback, delay);
+setTimeout(callback, delay);
+setTimeout(callback, delay);
+// implement clearAllTimeout();
+
+// Solution
+
+let oldTimeout = window.setTimeout;
+let allTimeout = [];
+
+window.setTimeout = function(callback, delay) {
+	let id = oldTimeout(callback, delay);
+	allTimeout.push(id);
+	return id;
+}
+
+function creallAllTimeout() {
+	allTimeout.forEach(id => clearTimeout(id));
+	allTimeout = [];
+}
+
---
+++ test.js
@@ -0,0 +1,22 @@
+const vehicles = [
+    { Make: 'Volkswagen', Brand: 'Passat', Color: 'Black', Number: 5 },
+    { Make: 'Toyota', Brand: 'Corolla', Color: 'Black', Number: 3 },
+    { Make: 'BMW', Brand: 'Mini', Color: 'red', Number: 6 },
+    { Make: 'Buick', Brand: 'Encore', Color: 'White', Number: 10 },
+    { Make: 'Ford', Brand: 'Explorer', Color: 'Sliver', Number: 7 },
+];
+
+let countItem = (arr) => {
+    let res = []
+    arr.map(item => Object.values(item).forEach(singleItem => res.push(singleItem)))
+    const count = res.reduce((allElements, ele) => {
+        if (ele in allElements) {
+            allElements[ele]++;
+        } else {
+            allElements[ele] = 1;
+        }
+        return allElements;
+    }, {});
+    return count;
+}
+console.log(countItem(vehicles));
