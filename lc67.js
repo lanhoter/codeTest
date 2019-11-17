9230e5532ee5ae17a9c80df045bc90b6c6adc633
lanhoter <lanhoter@hotmail.com>
2019-11-17 17:07:12 +0000

    Added lc9

--- lc67.js
+++ lc67.js
@@ -29,6 +29,13 @@ const addBinary = function (a, b) {
     return res;
 };
 
+
+// using the proposed BigInt object.
+// The idea is to use inputs, a and b to build two binary literals.
+// Calculating the sum is done by calling the BigInt function on our binary literals,
+// adding them together and returning the sum with a call to the toString method with 2 as the argument,
+// since we are working with binary numbers.
+
 const addBinary2 = function (a, b) {
     const aBin = `0b${a}`
     const bBin = `0b${b}`
