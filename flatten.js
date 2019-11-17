48fe921a46c3b95c2ce4fdfb4ef4d03a3a86285d
lanhoter <lanhoter@hotmail.com>
2019-11-17 16:44:27 +0000

    lc67

--- flatten.js
+++ flatten.js
@@ -1,9 +1,11 @@
 'use strict'
 
-const intialArr = [1, 2, 3, [4, 5, [6, 7, 8, [9, 10]], [10,11,[12, [13,14]]]]]
+const intialArr = [1, 2, 3, [4, [5], [6, [7, 8], [9, 10]], [10, 11, [12, [13, 14]]]]]
+
+let arrays = [[1, 2, 3], [4, 5], [6]];
 
 // flatten one level
-function flat(arr){
+function flat(arr) {
     let res = [];
     for (let i = 0; i < arr.length; i++) {
         Array.isArray(arr[i]) ? res = res.concat(flat(arr[i])) : res.push(arr[i])
@@ -12,7 +14,7 @@ function flat(arr){
 }
 
 // deep flatten
-function flatDeep(array, deep){
+function flatDeep(array, deep) {
     let res = [];
     for (let i = 0; i < array.length; i++) {
         Array.isArray(array[i]) && deep >= 1 ? res = res.concat(flatDeep(array[i], deep - 1)) : res.push(array[i])
@@ -21,7 +23,11 @@ function flatDeep(array, deep){
 }
 
 // flatten js
-const flatten = arr => arr.reduce((acc, cur) => (Array.isArray(cur) ? [...acc, ...flatten(cur)] : [...acc, cur]), [])
+// const flatten = arr => arr.reduce((acc, cur) => (Array.isArray(cur) ? [...acc, ...flatten(cur)] : [...acc, cur]), [])
 
+const flatten = arr => arr.reduce((acc, cur) => (Array.isArray(cur) ? acc.concat(flatten(cur)) : acc.concat(cur)), [])
+
+
+console.log(flat(arrays))
 console.log(flatDeep(intialArr, 4))
 console.log(flatten(intialArr))
---
+++ lc67.js
@@ -0,0 +1,45 @@
+// Given two binary strings, return their sum (also a binary string).
+//
+//
+//
+// For example,
+// a = "11"
+// b = "1"
+// Return "100".
+
+
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+const addBinary = function (a, b) {
+    let carry = 0;
+    let len1 = a.length;
+    let len2 = b.length;
+    let res = '';
+    while (len1 > 0 || len2 > 0) {
+        let sum = (+a[len1 - 1] || 0) + (+b[len2 - 1] || 0) + carry;
+        carry = Math.floor(sum / 2);
+        res = sum % 2 + res;
+        len1--;
+        len2--;
+    }
+    if (carry) res = '1' + res;
+    return res;
+};
+
+const addBinary2 = function (a, b) {
+    const aBin = `0b${a}`
+    const bBin = `0b${b}`
+    const sum = BigInt(aBin) + BigInt(bBin)
+    return sum.toString(2)
+};
+
+
+console.log(addBinary("11", "1"))
+console.log(addBinary("1010", "1011"))
+
+
+console.log(addBinary2("11", "1"))
+console.log(addBinary2("1010", "1011"))
