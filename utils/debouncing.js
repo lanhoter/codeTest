4bb4dac4b8766680291e64b10a0b64103ede80db
mdu <mdu@mdus-MBP.home>
2020-09-26 11:44:24 +0100

    Added eslint

---
+++ utils/debouncing.js
@@ -0,0 +1,19 @@
+function debounce(func, ms = 1000) {
+  let timer;
+  return function (...args) {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, ms);
+  };
+}
+
+// test
+
+const task = () => {
+  console.log("run task");
+};
+const debounceTask = debounce(task, 1000);
+window.addEventListener("scroll", debounceTask);
